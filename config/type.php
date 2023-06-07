992c39bf538878031ae8e924638659332c9ad3e1
vitodepi16 <118888625+vitodepi16@users.noreply.github.com>
2023-06-07 14:45:48 +0200

    create db & controller/model

---
+++ config/type.php
@@ -0,0 +1,50 @@
+<?php
+return [
+    "technologies" => [
+        [
+            "name" => "HTML",
+            "image" => "html-logo.png",
+            "description" => "HTML (HyperText Markup Language) è il linguaggio standard per la creazione di pagine web. Viene utilizzato per strutturare il contenuto delle pagine web utilizzando elementi come titoli, paragrafi, tabelle e immagini."
+        ],
+        [
+            "name" => "CSS",
+            "image" => "css-logo.png",
+            "description" => "CSS (Cascading Style Sheets) è un linguaggio utilizzato per definire lo stile e la formattazione dei documenti HTML. Viene utilizzato per controllare l'aspetto e il layout delle pagine web."
+        ],
+        [
+            "name" => "JavaScript",
+            "image" => "javascript-logo.png",
+            "description" => "JavaScript è un linguaggio di programmazione ad alto livello, interpretato e orientato agli oggetti. Viene principalmente utilizzato per aggiungere interattività agli elementi delle pagine web."
+        ],
+        [
+            "name" => "PHP",
+            "image" => "php-logo.png",
+            "description" => "PHP (Hypertext Preprocessor) è un linguaggio di scripting ampiamente utilizzato per lo sviluppo di applicazioni web dinamiche. È incorporato direttamente nel codice HTML e può interagire con database per gestire dati dinamici."
+        ],
+        [
+            "name" => "Laravel",
+            "image" => "laravel-logo.png",
+            "description" => "Laravel è un framework PHP open-source che facilita lo sviluppo di applicazioni web complesse. Fornisce un'architettura pulita e organizzata, insieme a un'ampia gamma di funzionalità pronte all'uso."
+        ],
+        [
+            "name" => "Bootstrap 5",
+            "image" => "bootstrap-logo.png",
+            "description" => "Bootstrap 5 è un framework CSS gratuito e open-source che semplifica la creazione di siti web responsive e moderni. Offre una vasta gamma di componenti e stili predefiniti per velocizzare lo sviluppo front-end."
+        ],
+        [
+            "name" => "Vite",
+            "image" => "vite-logo.png",
+            "description" => "Vite è un bundler JavaScript leggero e veloce che semplifica lo sviluppo delle applicazioni web. È progettato per migliorare il tempo di compilazione e l'esperienza di sviluppo grazie al suo approccio di caricamento dei moduli basato sugli ESM (ECMAScript Modules)."
+        ],
+        [
+            "name" => "Vue",
+            "image" => "vue-logo.png",
+            "description" => "Vue.js è un framework JavaScript progressivo per la creazione di interfacce utente. È utilizzato per la creazione di applicazioni web interattive e reattive, consentendo la gestione efficace dello stato e la manipolazione del DOM."
+        ],
+        [
+            "name" => "SASS (SCSS)",
+            "image" => "sass-logo.png",
+            "description" => "SASS (Syntactically Awesome Style Sheets) è un'estensione di CSS che aggiunge funzionalità avanzate come le variabili, i mixin e le strutture di controllo. La versione SCSS è la sintassi più recente e permette di utilizzare la familiarità del CSS tradizionale con l'aggiunta di potenti funzionalità."
+        ]
+    ]
+];
